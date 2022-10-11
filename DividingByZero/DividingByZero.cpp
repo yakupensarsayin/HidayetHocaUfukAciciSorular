f2a82b5d09f75a520f3aaf383f6e6d227681c624
Yakup Ensar Sayın <yakupensarsayin@hotmail.com>
2022-10-11 18:46:06 +0300

    Add project files.

---
+++ DividingByZero/DividingByZero.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <locale.h>
+
+float MakeDivisionViaFloatFunction(float numerator, float denominator)
+{
+    return (numerator / denominator);
+}
+
+int MakeDivisionViaIntFunction(int numerator, int denominator)
+{
+    return (numerator / denominator);
+}
+
+int main()
+{
+    int a = 5, b = 0;
+    
+    std::cout << MakeDivisionViaFloatFunction(a, b); 
+    // Outputs : "inf"
+    
+    std::cout << MakeDivisionViaIntFunction(a, b);
+    // MakeDivisionByIntFunction can not divide by zero. 
+    // Gives error code: 0xC0000094, Integer division by zero.
+}
