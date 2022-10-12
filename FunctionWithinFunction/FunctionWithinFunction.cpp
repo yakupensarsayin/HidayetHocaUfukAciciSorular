b16cbbcb656cb6d7ae8ef6b4166e95d684670fce
Yakup Ensar Sayın <yakupensarsayin@hotmail.com>
2022-10-12 12:26:22 +0300

    add features

---
+++ FunctionWithinFunction/FunctionWithinFunction.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+double AddTwoNumbers(double number1, double number2)
+{
+    return number1 + number2;
+}
+
+double TakeSquareOfGivenNumber(double number)
+{
+    return number * number;
+}
+
+int main()
+{
+    double a = 15.3;
+    double b = 2.55;
+    std::cout << TakeSquareOfGivenNumber(AddTwoNumbers(a, b));
+}
